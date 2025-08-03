0c680bcedbf1da3aef706783d3b31f214cc0353f
joshua odedeyi <hollyray2003@gmail.com>
2025-08-04 00:41:39 +0100

    excercise 3 authentication 4

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from .views import list_books
 from .views import book_list
 from . import views
@@ -13,3 +15,14 @@ urlpatterns = [
     # The '<pk>' captures the primary key from the URL and passes it to the view.
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
+
+# URL for user registration
+path('register/', views.register, name='register'),
+    
+    # URL for user login, using Django's built-in LoginView.
+    # We specify the template name that this view should render.
+path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    
+    # URL for user logout, using Django's built-in LogoutView.
+    # The 'next_page' argument redirects the user after a successful logout.
+path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
