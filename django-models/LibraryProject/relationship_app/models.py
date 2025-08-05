23cd0657905f7e03c7b44ef42a005eed6f5696bf
joshua odedeyi <hollyray2003@gmail.com>
2025-08-05 08:59:20 +0100

    help

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,3 +1,4 @@
+# relationship_app/models.py
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -5,33 +6,47 @@ from django.dispatch import receiver
 
 # Create your models here.
 class Author(models.Model):
-     """
+    """
     Represents an Author model with name.
     """
-     name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
 
-     
+    def __str__(self):
+        """
+        Returns the string representation of the Author model, which is its name.
+        This is a best practice for Django models.
+        """
+        return self.name
 
 class Book(models.Model):
-      """
+    """
     Represents a book with a title, author.
     """
-      title = models.CharField(max_length=200)
-      author = models.ForeignKey(Author,on_delete=models.CASCADE, related_name='author')
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author_books') # Changed related_name to avoid conflict
+
+    def __str__(self):
+        return self.title
 
 class Library(models.Model):
-       """
+    """
     Represents a library model with name and books.
     """
-       name = models.CharField(max_length=200)
-       books = models.ManyToManyField(Book, related_name='libraries')
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
+    def __str__(self):
+        return self.name
 
 class Librarian(models.Model):
-       """
+    """
     Represents a librarian model with a name of the librarian and the library.
     """
-       name = models.CharField(max_length=200)
-       library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 # Define role choices for the UserProfile
@@ -41,36 +56,37 @@ class UserRole(models.TextChoices):
     MEMBER = 'Member', 'Member'
 
 class UserProfile(models.Model):
-      """ Extends the Django User model to include a role for the user.
+    """ Extends the Django User model to include a role for the user.
     Uses a OneToOneField to link to Django's built-in User model.
     """
-      user = models.OneToOneField(User, on_delete=models.CASCADE)
-      role = models.CharField(max_length=10,
-                              choices=UserRole.choices,
-                              default=UserRole.MEMBER, #Default Role for Users
-                              help_text="User's assigned role in the system."
-                              )
-      
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(
+        max_length=10,
+        choices=UserRole.choices,
+        default=UserRole.MEMBER, #Default Role for Users
+        help_text="User's assigned role in the system."
+    )
+
+    def __str__(self):
+        """
+        Returns a string representation of the UserProfile,
+        combining the username and their role.
+        """
+        return f'{self.user.username} - {self.role}'
+
 # Signal to automatically create a UserProfile when a new User is created
 @receiver(post_save, sender=User)
-def create_or_update_user_profile(sender,instance,created, **kwargs):
-      """
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
     This signal receiver automatically creates a UserProfile for a new User.
     If the User already exists, it ensures the UserProfile is saved.
     """
-      if created:
-            # If a new user is created, create a corresponding UserProfile
-            UserProfile.objects.create(user = instance)
-      # Always save the UserProfile to ensure it's updated if the User is updated
-      instance.userprofile.save()
+    if created:
+        # If a new user is created, create a corresponding UserProfile
+        UserProfile.objects.create(user=instance)
+    # Always save the UserProfile to ensure it's updated if the User is updated
+    # This check ensures we only try to save if the userprofile attribute exists
+    # which it will after the create() call or if it's an existing user.
+    if hasattr(instance, 'userprofile'):
+        instance.userprofile.save()
 
-      
-
-
-
-def __str__(self):
-        """
-        Returns the string representation of the Author model, which is its name.
-        This is a best practice for Django models.
-        """
-        return self.name
