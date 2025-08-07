86dbcbc279df0435489e2bad74c79e7711ab875a
joshua odedeyi <hollyray2003@gmail.com>
2025-08-07 15:41:15 +0100

    corrected comma

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 # relationship_app/views.py
-from django.shortcuts import render,
-from django.shortcuts import redirect,
+from django.shortcuts import render
+from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import user_passes_test
@@ -100,15 +100,15 @@ def admin_view(request):
     # Check if the user is an Admin
     if is_admin(request.user):
         return render(request, 'admin_view.html', {'message': 'Welcome, Admin!'})
-    # elif is_librarian(request.user):
+    elif is_librarian(request.user):
         # If not Admin, but is Librarian, redirect to Librarian dashboard
-    #    return redirect('librarian_view')
-    # elif is_member(request.user):
+        return redirect('librarian_view')
+    elif is_member(request.user):
         # If not Admin or Librarian, but is Member, redirect to Member dashboard
-    #    return redirect('member_view')
-    # else:
+        return redirect('member_view')
+    else:
         # If authenticated but none of the defined roles, deny access
-    #    return HttpResponseForbidden("You do not have permission to access this page.")
+        return HttpResponseForbidden("You do not have permission to access this page.")
 
 @user_passes_test(is_librarian, login_url='/login/')
 def librarian_view(request):
