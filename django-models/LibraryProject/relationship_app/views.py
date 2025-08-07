8bd75b1f5f263f0a57cd183fab01ff65fb56f172
joshua odedeyi <hollyray2003@gmail.com>
2025-08-07 15:38:02 +0100

    testings 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -99,17 +99,16 @@ def admin_view(request):
     """
     # Check if the user is an Admin
     if is_admin(request.user):
-        # return render(request, 'admin_view.html', {'message': 'Welcome, Admin!'})
-        return render(request, 'admin_view.html')
-    elif is_librarian(request.user):
+        return render(request, 'admin_view.html', {'message': 'Welcome, Admin!'})
+    # elif is_librarian(request.user):
         # If not Admin, but is Librarian, redirect to Librarian dashboard
-        return redirect('librarian_view')
-    elif is_member(request.user):
+    #    return redirect('librarian_view')
+    # elif is_member(request.user):
         # If not Admin or Librarian, but is Member, redirect to Member dashboard
-        return redirect('member_view')
-    else:
+    #    return redirect('member_view')
+    # else:
         # If authenticated but none of the defined roles, deny access
-        return HttpResponseForbidden("You do not have permission to access this page.")
+    #    return HttpResponseForbidden("You do not have permission to access this page.")
 
 @user_passes_test(is_librarian, login_url='/login/')
 def librarian_view(request):
