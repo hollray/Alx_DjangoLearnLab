eda553ec8622a285f069b8da89e0ada63c8ffb39
joshua odedeyi <hollyray2003@gmail.com>
2025-08-15 20:45:14 +0100

    more test on week 10

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -17,10 +17,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from relationship_app import views as relationship_app_views
 
+
+# urlpatterns = [
+#    path('admin/', admin.site.urls),  # <-- default
+   
+#    path('', include('relationship_app.urls')),  # <-- Added this line myself
+#]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # <-- default
-   
-    path('', include('relationship_app.urls')),  # <-- Added this line myself
+    path('admin/', admin.site.urls), # < --- Default
+    path('', relationship_app_views.list_books, name='home'),
+    #path('', include('relationship_app.urls')),  # <-- Added this line myself
+    path('register/', relationship_app_views.register, name='register'),
+#path('login/', relationship_app_views.login_view, name='login'), # Assuming login_view exists
+    #path('logout/', relationship_app_views.logout_view, name='logout'), # Assuming logout_view exists
+    path('list_books/', relationship_app_views.list_books, name='list_books'),
+    path('admin-view/', relationship_app_views.admin_view, name='admin_view'),
+    path('librarian-view/', relationship_app_views.librarian_view, name='librarian_view'),
+    path('member-view/', relationship_app_views.member_view, name='member_view'),
+    path('add_book/', relationship_app_views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', relationship_app_views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', relationship_app_views.delete_book, name='delete_book'),
+    path('libraries/<int:pk>/', relationship_app_views.LibraryDetailView.as_view(), name='library_detail'),
 ]
