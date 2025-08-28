027b637cd0d7cf9bde81801a68fab48c07cdd6e2
joshua odedeyi <hollyray2003@gmail.com>
2025-08-28 11:35:55 +0100

    2nd Excercise on Week 15, worked on Post app correction test

---
+++ social_media_api/posts/README.md
@@ -0,0 +1,13 @@
+you can test your endpoints using Postman or a similar tool.
+
+POST to api/posts/ to create a post.
+
+GET api/posts/ to view all posts with pagination.
+
+GET api/posts/?search=your_query to search posts.
+
+GET api/posts/1/ to retrieve a single post.
+
+PUT/PATCH api/posts/1/ to update a post (only if you are the author).
+
+DELETE api/posts/1/ to delete a post (only if you are the author).
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+# posts/views.py
 
-# Create your views here.
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for handling Post-related API operations.
+    Provides CRUD functionality for posts and applies search and filtering.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['author']
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        """
+        Sets the author of the post to the current user upon creation.
+        """
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for handling Comment-related API operations.
+    Provides CRUD functionality for comments.
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        """
+        Sets the author of the comment to the current user.
+        """
+        serializer.save(author=self.request.user)
